4e5c089ec0c6e7314b4b063bae5cc310ae83f387
Michal Boska <michal.boska@erni.sk>
2014-12-08 23:16:05 +0100

    Client side work in progress

--- src/main/resources/www/app.js
+++ src/main/resources/www/app.js
@@ -1,17 +1,57 @@
-function Game() {
-    var Q = Quintus({development: true}).include("Sprites, Scenes, Input, 2D, Touch, UI").setup("game").controls().touch();
-    Q.Sprite.extend("Player1")
+window.onload = function() {
+    var Q = Quintus({development: true}).include("Sprites, Scenes, Input, 2D, Touch, UI").setup("game").controls(true).touch();
 
+    function playerObject(playerNum) {
+        var result = {
+            init: function (p) {
+                this._super(p, {
+                    asset: "black.png",
+                    x: playerNum == 1 ? 10 : 570,
+                    y: 250,
+                    w: 20,
+                    h: 100
+                });
 
-    this.lala = 2 + trrr() + new Nested().getPrrr();
-    function trrr() {
-        return 1;
-    }
-
-    function Nested() {
-        this.getPrrr = function() {
-            return 42;
+                this.add("2d");
+            },
+            bump: function() {
+                console.log("bump");
+            }
         }
+        if (playerNum == 1) {
+            result.step = function () {
+                var vy = Q.inputs['up'] ? -200 : 0;
+                vy =  Q.inputs['down'] ? 200 : vy;
+                var vySingle = vy / 800;
+                if (this.p.y + vySingle >= 0 && this.p.y + vySingle <= 600 - this.p.h) {
+                    this.p.vy = vy;
+                    console.log(this.p.y);
+                } else {
+                    this.p.vy = 0;
+                }
+            };
+        }
+        return result;
     }
-}
-new Game();
+
+    Q.Sprite.extend("Player1", playerObject(1));
+    Q.Sprite.extend("Player2", playerObject(2));
+
+
+    Q.scene("mainScene", function (stage) {
+        var player1 = stage.insert(new Q.Player1);
+        var player2 = stage.insert(new Q.Player2);
+    });
+
+    Q.gravityX = 0;
+    Q.gravityY = 0;
+    Q.input.keyboardControls();
+
+    Q.load("black.png",
+        function () {
+            Q.stageScene("mainScene");
+        }
+    );
+};
+
+
