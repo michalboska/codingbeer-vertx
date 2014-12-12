76011cf569e75fa500b45bfb8688d687bfc68f8c
Michal Boska <michal.boska@erni.sk>
2014-12-12 13:59:39 +0100

    ListPlayersDTO fix

--- src/main/java/ch/erni/beer/vertx/dto/lobby/ListPlayersDTO.java
+++ src/main/java/ch/erni/beer/vertx/dto/lobby/ListPlayersDTO.java
@@ -1,7 +1,22 @@
 package ch.erni.beer.vertx.dto.lobby;
 
+import ch.erni.beer.vertx.dto.PongDTO;
+import org.vertx.java.core.json.JsonArray;
+
+import java.util.Set;
+
 /**
  * Created by Michal on 6. 12. 2014.
  */
-public class ListPlayersDTO {
+public class ListPlayersDTO extends PongDTO {
+
+    public ListPlayersDTO(Set<String> playerNames) {
+        putArray("players", new JsonArray(playerNames.toArray()));
+        setStatusOk();
+    }
+
+    @Override
+    public String getType() {
+        return "listPlayers";
+    }
 }
